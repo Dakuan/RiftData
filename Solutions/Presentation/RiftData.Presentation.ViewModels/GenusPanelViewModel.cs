ca5f2393af3734cdde0232071093ed8fccd54e8d
Dakuan <dom.barker808@gmail.com>
2011-08-08 17:01:57 +0100

    sorted the tabstrip

---
+++ Solutions/Presentation/RiftData.Presentation.ViewModels/GenusPanelViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RiftData.Presentation.ViewModels.Dto;
+
+namespace RiftData.Presentation.ViewModels
+{
+    public class GenusPanelViewModel
+    {
+        public IList<GenusDto> GenusList;
+
+        public GenusTypeDto GenusType;
+
+        public int ExpandedGenus { get; set; }
+
+        public int SelectedSpecies { get; set; }
+    }
+}
