d1a0c79fde4a0d3d1021ae7aaa5eb59736885586
Dakuan <dom.barker808@gmail.com>
2011-08-08 22:43:02 +0100

    rebase refs

--- Solutions/Infrastructure/RiftData.Infrastructure.Data/DataContextInstaller.cs
+++ Solutions/Infrastructure/RiftData.Infrastructure.Data/DataContextInstaller.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Castle.Core;
+﻿using Castle.Core;
 using Castle.MicroKernel.Registration;
 using Castle.MicroKernel.SubSystems.Configuration;
 using Castle.Windsor;
