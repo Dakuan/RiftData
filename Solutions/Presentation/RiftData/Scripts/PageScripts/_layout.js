43501695fff345c1a01329a7014acd7cf7ae76bc
Dakuan <dom.barker808@gmail.com>
2011-08-18 08:44:28 +0100

    stylin

---
+++ Solutions/Presentation/RiftData/Scripts/PageScripts/_layout.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    var hoverAnimationTime = 100;
+    $('.headerButton').hover(function () {
+        $(this).switchClass('headerButton', 'headerButtonHover', hoverAnimationTime);
+    });
+    $('.headerButton').mouseout(function () {
+        $(this).switchClass('headerButtonHover', 'headerButton', hoverAnimationTime);
+    });
+});
