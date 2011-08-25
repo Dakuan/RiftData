028e816e72fa42335bf2536443590bfe1ea25d32
Dakuan <dom.barker808@gmail.com>
2011-08-25 17:53:18 +0100

    modified service to not retun species within genus

--- Solutions/Presentation/RiftData/Controllers/ServiceController.cs
+++ Solutions/Presentation/RiftData/Controllers/ServiceController.cs
@@ -2,6 +2,8 @@
 
 namespace RiftData.Controllers
 {
+    using System.Linq;
+
     using RiftData.ApplicationServices.DtoServices.Contracts;
 
     public class ServiceController : Controller
@@ -15,6 +17,10 @@ namespace RiftData.Controllers
 
         public ActionResult GetAllGenera()
         {
+            var results = this.genusDtoService.GetGenusDtos();
+
+            results.ToList().ForEach(g => g.Species.Clear());
+
             return new JsonResult { JsonRequestBehavior = JsonRequestBehavior.AllowGet, Data = this.genusDtoService.GetGenusDtos() };
         }
     }
