99452337eaec6d6a3da1081f858022d3a490500a
unknown <Dom@.earthware.local>
2011-08-25 16:52:08 +0100

    added service controller

---
+++ Solutions/Presentation/RiftData/Controllers/ServiceController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace RiftData.Controllers
+{
+    using RiftData.ApplicationServices.DtoServices.Contracts;
+
+    public class ServiceController : Controller
+    {
+        private readonly IGenusDtoService genusDtoService;
+
+        public ServiceController(IGenusDtoService genusDtoService)
+        {
+            this.genusDtoService = genusDtoService;
+        }
+
+        public ActionResult GetAllGenera()
+        {
+            return new JsonResult { JsonRequestBehavior = JsonRequestBehavior.AllowGet, Data = this.genusDtoService.GetGenusDtos() };
+        }
+    }
+}
