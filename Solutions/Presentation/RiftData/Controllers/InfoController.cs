a03aa43752ecfb8d84373e2251d7d95bf874014e
Dakuan <dom.barker808@gmail.com>
2011-09-19 23:06:35 +0100

    added metadata for static pages

--- Solutions/Presentation/RiftData/Controllers/InfoController.cs
+++ Solutions/Presentation/RiftData/Controllers/InfoController.cs
@@ -1,5 +1,9 @@
-﻿namespace RiftData.Controllers
+﻿using RiftData.ApplicationServices.DtoServices.Contracts;
+
+namespace RiftData.Controllers
 {
+    using System.Linq;
+
     using System.Web.Mvc;
 
     using RiftData.Presentation.Contracts;
@@ -7,15 +11,23 @@
     public class InfoController : Controller
     {
         private readonly IInfoPageViewModelFactory infoPageViewModelFactory;
+        private readonly IGenusTypeDtoService genusTypeDtoService;
+        private readonly ILakeDtoService lakeDtoService;
 
-        public InfoController(IInfoPageViewModelFactory infoPageViewModelFactory)
+        public InfoController(IInfoPageViewModelFactory infoPageViewModelFactory, ILakeDtoService lakeDtoService, IGenusTypeDtoService genusTypeDtoService)
         {
             this.infoPageViewModelFactory = infoPageViewModelFactory;
+            this.genusTypeDtoService = genusTypeDtoService;
+            this.lakeDtoService = lakeDtoService;
         }
 
         public ActionResult About()
         {
             var viewModel = this.infoPageViewModelFactory.Build();
+
+            viewModel.Description = "Information about Rift Data";
+
+            viewModel.Keywords = this.AssembleKeywords();
 
             return View(viewModel);
         }
@@ -24,7 +36,18 @@
         {
             var viewModel = this.infoPageViewModelFactory.Build();
 
+            viewModel.Description = "Help us improve RiftData";
+
+            viewModel.Keywords = this.AssembleKeywords();
+
             return View(viewModel);
+        }
+
+        private string AssembleKeywords()
+        {
+            var keywords = string.Format("Rift Valley Cichlids, {0}, {1}", string.Join(", ", this.lakeDtoService.GetAllLakes().Select(x => x.Name)), string.Join(", ", this.genusTypeDtoService.GetAllGenusTypes().Select(x => x.Name)));
+
+            return keywords;
         }
     }
 }
