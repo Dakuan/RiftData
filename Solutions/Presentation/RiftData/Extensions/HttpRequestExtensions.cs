4a64668d1cb0e77e12e119f57d4c4574c4afba97
Dakuan <dom.barker808@gmail.com>
2011-09-19 18:12:35 +0100

    added cannonical rel link thing

---
+++ Solutions/Presentation/RiftData/Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace RiftData.Extensions
+{
+    using System;
+    using System.Web;
+
+    public static class HttpRequestExtensions
+    {
+        public static Uri UrlOriginal(this HttpRequestBase request)
+        {
+            var hostHeader = request.Headers["host"];
+
+            return new Uri(
+                string.Format(
+                    "{0}://{1}{2}",
+                    request.Url.Scheme,
+                    hostHeader,
+               request.RawUrl));
+        }
+    }
+}
