cce40879e319da3425d4e082544a5db87c84aef8
Dakuan <dom.barker808@gmail.com>
2011-09-28 14:03:04 +0100

    added google plus button to sharing thing

--- Solutions/Presentation/RiftData/Scripts/Mobile/Mobile.js
+++ Solutions/Presentation/RiftData/Scripts/Mobile/Mobile.js
@@ -3,7 +3,10 @@ $("#RiftDataPage").live('pagecreate', function () {
     if ($("#Gallery a").length > 0) {
         $("#Gallery a").photoSwipe({ enableMouseWheel: false, enableKeyboard: false });
     }
-    $('#SocialMediaButtons').jsShare({ maxwidth: 259, initialdisplay: 'expanded', yoursitename: 'RiftData', desc: 'Rift valley cichlid database', yoursitetitle: 'RiftData', delicious: false, linkedin: false, googlebuzz: false });
+    $('#SocialMediaButtons').jsShare({ maxwidth: 300, initialdisplay: 'expanded', yoursitename: 'RiftData', desc: 'Rift valley cichlid database', yoursitetitle: 'RiftData', delicious: false, linkedin: false, googlebuzz: false });
+
+    $('<li><div style="margin-top:8px;"><g:plusone size="small" annotation="none"></g:plusone></div></li>').appendTo('#SocialMediaButtons ul');
+
 });
 
 $(document).bind('mobileinit', function () {
