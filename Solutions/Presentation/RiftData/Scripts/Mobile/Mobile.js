216b9137fac371caaf837eff06c7f2dce008084e
Dakuan <dom.barker808@gmail.com>
2011-09-28 12:55:11 +0100

    added social media buttons to mobile site

--- Solutions/Presentation/RiftData/Scripts/Mobile/Mobile.js
+++ Solutions/Presentation/RiftData/Scripts/Mobile/Mobile.js
@@ -3,6 +3,7 @@ $("#RiftDataPage").live('pagecreate', function () {
     if ($("#Gallery a").length > 0) {
         $("#Gallery a").photoSwipe({ enableMouseWheel: false, enableKeyboard: false });
     }
+    $('#SocialMediaButtons').jsShare({ maxwidth: 360, initialdisplay: 'expanded', yoursitename: 'RiftData', desc: 'Rift valley cichlid database', yoursitetitle: 'RiftData' });
 });
 
 $(document).bind('mobileinit', function () {
